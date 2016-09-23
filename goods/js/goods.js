c72b5159f05c1abacf37f095f2aec33298ad4e8b
Ruonan Liu <lrn_sy@126.com>
2016-09-23 09:47:51 +0800

    Add files via upload

---
+++ goods/js/goods.js
@@ -0,0 +1,28 @@
+window.onload=function(){
+	function f () {
+	  var Div=document.getElementById('main1').getElementsByClassName('pic');
+	return Div.length;
+	}
+	f();
+	function imgDisplay() {
+		var Div = document.getElementById('main1').getElementsByClassName('pic');
+		
+		var divHeight1 = 400;
+		var divHeight2 = 340;
+		for (var i = 0; i < Div.length; i++) {
+			Div[i].onmouseover = showMeg;
+			Div[i].onmouseout = hideMeg;
+		}
+		function showMeg() {
+			this.getElementsByTagName('a')[0].style.top = divHeight2 + 'px';
+		}
+
+		function hideMeg() {
+			this.getElementsByTagName('a')[0].style.top = divHeight1 + 'px';
+		}
+
+	}
+
+	imgDisplay();
+	
+};
